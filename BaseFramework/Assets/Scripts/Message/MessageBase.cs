b253d356053a8772decd5035c5f17722c9decd69
talentduanbin@163.com <htuDbb4073>
2017-09-02 19:32:54 +0800

    添加消息机制代码

---
+++ BaseFramework/Assets/Scripts/Message/MessageBase.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+
+public class MessageBase
+{
+    protected Hashtable arguments;
+    protected MessageType type;
+    protected object sender;
+    public MessageType Type
+    {
+        get
+        {
+            return this.type;
+        }
+        set
+        {
+            this.type = value;
+        }
+    }
+
+    public IDictionary Params
+    {
+        get
+        {
+            return this.arguments;
+        }
+        set
+        {
+            this.arguments = (value as Hashtable);
+        }
+    }
+
+    public object Sender
+    {
+        get
+        {
+            return this.sender;
+        }
+        set
+        {
+            this.sender = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        return this.type + "[" + ((this.sender == null) ? "null" : this.sender.ToString()) + "]";
+    }
+
+    public MessageBase Clone()
+    {
+        return new MessageBase(this.type, this.arguments, Sender);
+    }
+
+    public MessageBase(MessageType type, Object sender)
+    {
+        this.Type = type;
+        Sender = sender;
+        if (this.arguments == null)
+        {
+            this.arguments = new Hashtable();
+        }
+    }
+
+    public MessageBase(MessageType type, Hashtable args, Object sender)
+    {
+        this.Type = type;
+        this.arguments = args;
+        Sender = sender;
+        if (this.arguments == null)
+            this.arguments = new Hashtable();
+    }
+
+}
+
+
