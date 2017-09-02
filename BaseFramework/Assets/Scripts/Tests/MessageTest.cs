7bc7bef446b5cfc1a91045fb17a3d2aab27edb7d
talentduanbin@163.com <htuDbb4073>
2017-09-02 19:26:17 +0800

    添加使用Hashtable管理的表格

---
+++ BaseFramework/Assets/Scripts/Tests/MessageTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class MessageTest : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        MessageMgr.GetInstance().AddEventListener(MessageType.NEXT_BATTALE_START, BattleStart);
+        for (int i = 0; i < 1000; i++)
+        {
+            if (i >= 100)
+            {
+                MessageMgr.GetInstance().DispatchEvent(new MessageBase(MessageType.NEXT_BATTALE_START, this));
+                return;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void BattleStart(MessageBase cbe)
+    {
+        Debug.Log("GAME START");
+    }
+
+
+}
